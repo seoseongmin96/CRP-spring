26fa29ced23102b98b2c1a86353a848e1c5699ba
seoseongmin96 <justin090452@gmail.com>
2022-05-12 16:54:01 +0900

    람다 연습

--- src/main/java/crp/kr/api/common/lambda/Lambda.java
+++ src/main/java/crp/kr/api/common/lambda/Lambda.java
@@ -1,16 +1,17 @@
 package crp.kr.api.common.lambda;
 
 
+import javax.persistence.criteria.CriteriaBuilder;
+
 import static crp.kr.api.common.dataStructure.AppleList.Apple;
 
+import java.io.File;
 import java.util.Arrays;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Lambda {
     public static void main(String[] args) {
-        // System.out.println(Lambda.integer("900"));
+ /**       // System.out.println(Lambda.integer("900"));
         System.out.println(string(new Apple.Builder().origin("영동").color("RED").price(3000).build()));
         System.out.println(
                 string(
@@ -21,7 +22,10 @@ public class Lambda {
 
                 ))
         );
-        System.out.println(array(10).length);
+       System.out.println(array(10).length);*/
+        System.out.println(random(1,6));
+
+
     }
     public static int integer(String s){
         // Integer i = Integer.parseInt("900");
@@ -29,6 +33,7 @@ public class Lambda {
         return f.apply(s);
     }
     public static String  string(Object o){
+
         // String s = String.valueOf(o);
         Function<Object, String> f = String::valueOf;
         return f.apply(o);
@@ -41,7 +46,19 @@ public class Lambda {
         Function<Integer, int[]> f = int[]::new;
         return f.apply(a);
     }
-
     // int [] arr = new int [8];
     // = int[]::new
-}
+    // int p = random(1,6)
+
+    public static int random(int min, int max){
+      BiFunction<Integer, Integer, Integer> f = (t,u) -> (int)(Math.random()*u)+t;
+              return f.apply(min,max);
+    }
+// File file = new File("d:\\example\\file.txt")
+    public static File makeFile(String a){
+        Function<String ,File> f = File::new;
+        return f.apply(a);
+    }
+
+
+    }

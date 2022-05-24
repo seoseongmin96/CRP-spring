924840e2c77afa9e62717132a7253236dfe31008
seoseongmin96 <justin090452@gmail.com>
2022-05-24 15:37:01 +0900

    security study

--- src/main/java/crp/kr/api/auth/domains/AuthProvider.java
+++ src/main/java/crp/kr/api/auth/domains/AuthProvider.java
@@ -2,17 +2,27 @@ package crp.kr.api.auth.domains;
 
 import crp.kr.api.user.domains.Role;
 import crp.kr.api.auth.services.AuthServiceImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * packageName : crp.kr.api.security.domains
@@ -44,9 +54,26 @@ public class AuthProvider implements AuthenticationProvider {
     }
 
     public String createToken(String username, List<Role> roles){
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority()))
+                .filter(Objects::nonNull).collect(Collectors.toList()));
+        Date now = new Date();
+        Date validity = new Date(now.getTime()+validityInMs);
+        return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, securityKey)
+                .compact();
+    }
+    public Authentication getAuthentication(String token){
+        UserDetails auth = service.loadUserByUsername(token);
+        return new UsernamePasswordAuthenticationToken(auth.getAuthorities(),"",auth.getAuthorities());
+    }
+    public String getUsername(String token){
+        return  Jwts.parser().setSigningKey(securityKey).parseClaimsJws(token).getBody().getSubject();
+    }
+    public String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("");
         return "";
     }
-
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
